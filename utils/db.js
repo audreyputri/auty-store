d6e184bb976e3621c7bfdbfd0fc61d3c9c9e5dc1
audreyputri <83500997+audreyputri@users.noreply.github.com>
2021-07-28 15:53:32 +0700

    first 28/07/2021

---
+++ utils/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://muthiasyiffa:apayalupagua@cluster0.n0zvz.mongodb.net/autystore', 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+
+
+
